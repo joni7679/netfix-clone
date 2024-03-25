20b7744ae114041a1c4e740dd2d9860a596fd32e
joni <codingmylife0@gmail.com>
2024-03-25 17:30:15 +0530

    update and modifiy

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-let allQestions = document.querySelectorAll(".question-box");
-console.log(allQestions)
-allQestions.forEach((val, index) => {
+// let allQestions = document.querySelectorAll(".question-box");
+// console.log(allQestions)
+// allQestions.forEach((val, index) => {
+//     val.addEventListener('click', () => {
+//         val.nextElementSibling.classList.toggle('h-[auto]');
+//         val.nextElementSibling.classList.toggle("scale-y-100");
+//         val.nextElementSibling.classList.toggle('p-[20px]');
+//         // console.log(index)
+//         allQestions.forEach((elem, i) => {
+//             if (i != index) {
+//                 elem.nextSibling.classList.remove('h-[auto]', 'scale-y-100', 'p-[20px]')
+//             }
+//         })
+//     })
+
+// })
+
+
+let allQuestions = document.querySelectorAll(".question-box");
+console.log(allQuestions);
+
+allQuestions.forEach((val, index) => {
     val.addEventListener('click', () => {
         val.nextElementSibling.classList.toggle('h-[auto]');
         val.nextElementSibling.classList.toggle("scale-y-100");
         val.nextElementSibling.classList.toggle('p-[20px]');
-        allQestions.forEach((elem, i) => {
-            if (i != index) {
-                elem.nextSibling.classList.remove('h-[auto]', 'scale-y-100', 'p-[20px]')
+        val.nextElementSibling.style.transitionDuration = '400ms';
+        allQuestions.forEach((elem, i) => {
+            if (i !== index) {
+                elem.nextElementSibling.classList.remove('h-[auto]', 'scale-y-100', 'p-[20px]');
             }
-        })
-    })
-
-})
+        });
+    });
+});
